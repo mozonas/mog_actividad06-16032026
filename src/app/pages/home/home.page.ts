f8ad6cf273e68db69691c5aae963861543e9d662
Miguel <miguelozonas@gmail.com>
2026-03-16 14:07:51 +0100

    ya se empieza a ver el listado de usuarios, es decir, home y user-card +- ok

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal} from '@angular/core';
+import { Users } from '../../core/services/users';
+import { IUser } from '../../core/models/user.interface'; 
+import { UserCardComponent } from '../../shared/components/user-card/user-card.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [UserCardComponent],
   templateUrl: './home.page.html',
   styleUrl: './home.page.css',
 })
 export class HomePage {
+  private usersService = inject(Users);
+  users = signal<IUser[]>([]);
 
+  constructor() {
+    this.loadUsers();
+  }
+
+  async loadUsers() {
+    try {
+      const response = await this.usersService.getAll("");
+      this.users.set(response.results);
+      console.log(this.users());
+      console.log(response);
+    } catch (error) {
+      console.error('Error loading users:', error);
+    }
+  } 
 }
--- src/app/shared/components/user-card/user-card.component.ts
+++ src/app/shared/components/user-card/user-card.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component,input } from '@angular/core';
+import { IUser } from '../../../core/models/user.interface';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './user-card.component.html',
   styleUrl: './user-card.component.css',
 })
 export class UserCardComponent {
-
+   miPersonaje = input<IUser>()
 }
